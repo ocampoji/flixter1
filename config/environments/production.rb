da3662d595582ffc5ce6d3432677516daff744e5
Jorge Ocampo <ocajor1@hotmail.com>
2019-09-03 00:43:18 +0000

    Flixter Lesson 11-1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'flixter1-jorge-ocampo.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
